7e3a6de5fc9b2c0ea69e02464cd059266619af64
Catalina <catalinapiazza@gmail.com>
2020-01-10 17:12:26 -0300

    add script validator contact-form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -38,7 +38,7 @@ if( empty($errors))
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
 <html>
 <head>
-	<title>Contact form handler</title>
+	<title>Massif - Agencia de Marketing Digital y Diseño Web</title>
 </head>
 
 <body>
