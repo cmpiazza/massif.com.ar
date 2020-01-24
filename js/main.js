669f798c9f59dfebd5b14f75cf4d2c1eed332bf2
Catalina <catalinapiazza@gmail.com>
2020-01-24 14:06:59 -0300

    tiendas nube + boton

--- js/main.js
+++ js/main.js
@@ -219,3 +219,19 @@
   
 
 })(jQuery);
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Ver ejemplo";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Ocultar ejemplo";
+    moreText.style.display = "inline";
+  }
+}
